0a1218b53acd03a16a0328702058602fa756c299
davidebolo1993 <davidebolognini7@gmail.com>
2022-12-19 14:29:07 +0100

    adding conversion to uniprot and features extraction

--- workflow/scripts/tsvtomtx.r
+++ workflow/scripts/tsvtomtx.r
@@ -2,6 +2,9 @@
 
 library(Matrix)
 library(optparse)
+library(UniProt.ws)
+library(OmnipathR)
+
 sessionInfo()
 
 writeMMgz <- function(x, file) {
@@ -28,13 +31,13 @@ writeMMgz <- function(x, file) {
 	)
 }
 
-writeGzFile <- function(x,file){
+writeGzFile <- function(x,file,has_colnames){
 	data.table::fwrite(
 		x = x,
 		file = file,
 		sep = "\t",
 		row.names = FALSE,
-		col.names = FALSE
+		col.names = has_colnames
 	)
 }
 
@@ -44,7 +47,7 @@ option_list = list(
   	make_option(c('-c', '--counts'), action='store', type='character', help='gene/transcript counts .tsv'),
 	make_option(c('-g', '--gtf'), action='store', type='character', help='gene model in .gtf format'),
 	make_option(c('-o', '--output'), action='store', type='character', help='output directory'),
-    make_option(c('-t', '--transcript'), action='store_true', default=FALSE, help='use transcript matrix instead of gene matrix'),
+	make_option(c('-t', '--transcript'), action='store_true', default=FALSE, help='use transcript matrix instead of gene matrix'),
 	make_option(c('-b', '--biotype'), action='store_true', default=FALSE, help='additionally store a features-like file with biotypes')
 )
 
@@ -97,17 +100,19 @@ if (!opt$transcript) {
 	message('[',now,'][Message] done')
 	message('[',now,'][Message] storing to file')
 
-	writeGzFile(x = data.frame(V1=rownames(gbm),V2=vals, V3="Gene Expression"), file =file.path(opt$output,"features.tsv.gz"))
+	writeGzFile(x = data.frame(V1=rownames(gbm),V2=vals, V3="Gene Expression"), file =file.path(opt$output,"features.tsv.gz"),has_colnames=FALSE)
 
 	if (opt$biotype) {
 
 		now<-Sys.time()
 		message('[',now,'][Message] extracting biotypes')
 		bios<-do.call(c,lapply(rownames(gbm),function(x) {unique(gtf_df[gene_name == x]$gene_type)[1]}))
-        now<-Sys.time()
-        message('[',now,'][Message] done')
-        message('[',now,'][Message] storing to file')		
-		writeGzFile(x = data.frame(V1=rownames(gbm),V2=vals, V3=bios), file =file.path(opt$output,"biotypes.tsv.gz"))
+		now<-Sys.time()
+		message('[',now,'][Message] done')
+		message('[',now,'][Message] storing to file')
+		gns_df<-data.frame(V1=rownames(gbm),V2=vals, V3=bios)
+		colnames(gns_df)<-c("gene_name", "gene_id", "gene_type")	
+		writeGzFile(x = gns_df, file =file.path(opt$output,"biotypes.tsv.gz"),has_colnames=TRUE)
 	}
 
 
@@ -121,20 +126,47 @@ if (!opt$transcript) {
 	message('[',now,'][Message] done')
 	message('[',now,'][Message] storing to file')
 
-	writeGzFile(x = data.frame(V1=vals,V2=rownames(gbm), V3="Gene Expression"), file =file.path(opt$output,"features.tsv.gz"))
+	writeGzFile(x = data.frame(V1=vals,V2=rownames(gbm), V3="Gene Expression"), file =file.path(opt$output,"features.tsv.gz"),has_colnames=FALSE)
 
-        if (opt$biotype) {
+	if (opt$biotype) {
 
-                now<-Sys.time()
-                message('[',now,'][Message] extracting biotypes')
-                bios<-do.call(c,lapply(rownames(gbm),function(x) {unique(gtf_df[transcript_id == x]$transcript_type)[1]}))
-                now<-Sys.time()
-                message('[',now,'][Message] done')
-                message('[',now,'][Message] storing to file')
-                writeGzFile(x = data.frame(V1=vals,V2=rownames(gbm), V3=bios), file =file.path(opt$output,"biotypes.tsv.gz"))
-        }
+		now<-Sys.time()
+		message('[',now,'][Message] extracting biotypes')
+		bios<-do.call(c,lapply(rownames(gbm),function(x) {unique(gtf_df[transcript_id == x]$transcript_type)[1]}))
+		now<-Sys.time()
+		message('[',now,'][Message] done')
+		trns_df<-data.frame(V1=vals,V2=rownames(gbm), V3=bios)
+		message('[',now,'][Message] retrieving uniprot ids')
+		entrez_df<-as.data.frame(uniprot_id_mapping_table(trns_df$V2, from="enst", to="uniprot", chunk_size = NULL))
+		trns_df$V4 <- entrez_df$To[match(trns_df$V2, entrez_df$From)]
+		now<-Sys.time()
+		message('[',now,'][Message] done')
+		message('[',now,'][Message] querying uniprot db for features')
 
+		#query routine
+		up <- UniProt.ws(taxId=9606)
+		kt<-"UniProtKB"
+		columns<-c("cc_subcellular_location","ft_transmem","xref_ensembl")
+		res<-select(up,trns_df$V4,columns,kt)
+		res_df<-as.data.frame(res)
+		now<-Sys.time()
+		message('[',now,'][Message] done')
+		message('[',now,'][Message] matching informations and storing to file')
 
+		#subcellular location
+		trns_df$V5<-res_df$Subcellular.location..CC.[match(trns_df$V4, res_df$From)]
+		#transmembrane
+		trns_df$V6<-res_df$Transmembrane[match(trns_df$V4, res_df$From)]
+		#ensembl_ids
+		trns_df$V7<-res_df$Ensembl[match(trns_df$V4, res_df$From)]
+
+		#to None to facilitate downstream analysis in python
+		trns_df[is.na(trns_df)] <- "None"
+		colnames(trns_df)<-c("transcript_name", "transcript_id", "transcript_type", "protein_id", "cc_subcellular_location", "ft_transmem", "xref_ensembl")
+
+		#write
+		writeGzFile(x = trns_df, file =file.path(opt$output,"biotypes.tsv.gz"), has_colnames=TRUE)
+	}
 
 }
 
@@ -143,7 +175,7 @@ message('[',now,'][Message] done')
 message('[',now,'][Message] storing cell barcodes to file')
 
 #barcodes
-writeGzFile(x = data.frame(V1=paste0(colnames(gbm), "-1")), file=file.path(opt$output,"barcodes.tsv.gz"))
+writeGzFile(x = data.frame(V1=paste0(colnames(gbm), "-1")), file=file.path(opt$output,"barcodes.tsv.gz"), has_colnames=FALSE)
 
 now<-Sys.time()
 message('[',now,'][Message] done')
