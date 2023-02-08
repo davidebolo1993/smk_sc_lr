1a7f23e4007e52287d7a78dd2c46c93af61877e8
davidebolo1993 <davidebolognini7@gmail.com>
2023-02-08 15:48:45 +0100

    adding rules for running blaze - and env [ci skip]

---
+++ workflow/scripts/runblaze.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+#conda environemnt - load
+eval "$(conda shell.bash hook)"
+conda activate snakemakeenv_latest
+
+#run
+snakemake --unlock
+snakemake --profile config/slurm blaze_barcodes
